d9b23e0530a68d98812e30badb7ce0830edf4371
inogNation <87098776+inogNation@users.noreply.github.com>
2025-04-27 15:54:52 -0500

    Add files via upload

--- banner.js
+++ banner.js
@@ -1,5 +1,15 @@
 document.write('\
-               <h1>This header is made in JavaScript</h1>\
+              	\
+		<div class="pageBanner"></div>\
+		\
+		<ul class="menu">\
+				<a href="index.html"><li class="menuItem">Home</li></a>\
+				<a href="events.html"><li class="menuItem">Event Calendar</li></a>\
+				<a href="programs.html"><li class="menuItem">Programs</li></a>\
+				<a href="authors.html"><li class="menuItem">Authors & Artists</li></a>\
+		</ul>\
                \
+		<div class="pageUnderBanner"></div>\
                \
+		<br>\
                ');
